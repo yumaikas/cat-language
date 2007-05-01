52df21f45ef4b7dca51a56fa3d9ffeea9a7ee8b7
cdiggins <cdiggins@c3f59c99-e62b-0410-a2a3-eb39d943d493>
2007-05-01 18:06:52 +0000

    Version 0.12 release.

--- trunk/Main.cs
+++ trunk/Main.cs
@@ -31,7 +31,7 @@ namespace Cat
                 {
                     WriteLine("");
                     WriteLine("Cat Interpreter");
-                    WriteLine("version 0.12.0 April 30th, 2007");
+                    WriteLine("version 0.12.0 May 1st, 2007");
                     WriteLine("by Christopher Diggins");
                     WriteLine("this software is released under the MIT license");
                     WriteLine("the source code is public domain and available at");
